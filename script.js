6daf234f93b2248cb3dd400fa8f6d17af86347cf
“josegarglex72” <josegarglex@gmail.com>
2019-08-30 17:12:40 -0500

    Initial commit with Header buttons (close,min,maz=x) working fine

---
+++ script.js
@@ -0,0 +1,36 @@
+const {remote} = require('electron');
+
+var app = angular.module('app', ['ngRoute']);
+
+
+app.config(function($routeProvider){
+    $routeProvider.when('/',{
+        templateUrl : `${__dirname}/components/home/home.html`,
+        controller: 'homeCtrl'
+    }) 
+});
+
+app.controller('headCtrl', function($scope){
+
+    var win = remote.getCurrentWindow();
+
+    $scope.close = function(){
+
+        win.close();
+        
+    }
+    $scope.maximize = function(){
+
+        win.isMaximized() ? win.unmaximize() : win.maximize();
+    }
+    $scope.minimize = function(){
+
+        win.minimize();
+
+    }
+
+});
+
+app.controller('homeCtrl', function($scope){
+
+});
